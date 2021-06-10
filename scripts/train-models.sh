6065f24fb0c61f45cbc76d8191976dc03df93dc5
Ken Lee <keun.lee@thekeunster.com>
2021-06-09 23:28:06 -0500

    updated train models script

--- scripts/train-models.sh
+++ scripts/train-models.sh
@@ -6,6 +6,7 @@ export PORT=3306
 export DATABASE=beer_horoscope
 export USER=user
 export PASSWORD=password
+mkdir -p /mnt/storage/out
 source /home/temp/venv/bin/activate
 pip install -r /home/temp/beer-horoscope/src/training/requirements.txt
 python /home/temp/beer-horoscope/src/training/main.py
